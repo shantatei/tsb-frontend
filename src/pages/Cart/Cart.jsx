5f743351de99c2fffa0ecbdb7427577106a09ebc
shanta <teirenxuanshanta@gmail.com>
2022-08-21 22:11:24 +0800

    styled cart

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -2,24 +2,17 @@ import { React, useEffect } from "react";
 import {
   Container,
   Typography,
-  Stack,
   Grid,
   Button,
   Divider,
-  IconButton,
+  Stack,
+  Box,
 } from "@mui/material";
 import { useSelector, useDispatch } from "react-redux";
-import AddIcon from "@mui/icons-material/Add";
-import RemoveIcon from "@mui/icons-material/Remove";
-import {
-  addToCart,
-  decreaseCart,
-  emptyCart,
-  getTotals,
-  removeFromCart,
-} from "../../redux/cartSlice";
+import { emptyCart, getTotals } from "../../redux/cartSlice";
 import { Link } from "react-router-dom";
 import EmptyCartImage from "../../assets/Add to Cart-amico.png";
+import CartItem from "./components/CartItem";
 
 const Cart = () => {
   const cart = useSelector((state) => state.cart);
@@ -28,18 +21,6 @@ const Cart = () => {
   useEffect(() => {
     dispatch(getTotals());
   }, [cart, dispatch]);
-
-  const handleRemoveFromCart = (cartItem) => {
-    dispatch(removeFromCart(cartItem));
-  };
-
-  const handleDecreaseCart = (cartItem) => {
-    dispatch(decreaseCart(cartItem));
-  };
-
-  const handleIncreaseQuantity = (cartItem) => {
-    dispatch(addToCart(cartItem));
-  };
 
   const handleEmptyCart = () => {
     dispatch(emptyCart());
@@ -70,128 +51,77 @@ const Cart = () => {
   };
 
   const FilledCart = ({ cart }) => {
+    const line = {
+      position: "relative",
+      "&::after": {
+        content: "''",
+        width: "100%",
+        left: 0,
+        height: "2px",
+        position: "absolute",
+        bottom: "-5px",
+        backgroundImage: "linear-gradient(to right, #3396f5, #001A33)",
+        borderRadius: "10px",
+      },
+    };
+
     return (
       <>
-        <Grid container mt={3} mb={3}>
-          <Grid item md={4}>
-            <Typography>Product</Typography>
-          </Grid>
-          <Grid item md={2}>
-            <Typography>Price</Typography>
-          </Grid>
-          <Grid item md={4}>
-            <Typography>Quantity</Typography>
-          </Grid>
-          <Grid item md={2}>
-            <Typography>Total</Typography>
-          </Grid>
-        </Grid>
-        <Divider />
-        <Grid container mt={3} mb={3}>
-          {cart.cartItems?.map((cartItem) => (
-            <>
-              <Grid item md={4} display="flex" mt={2}>
-                <img
-                  style={{
-                    width: "50%",
-                  }}
-                  src={`http://localhost:8000/storage/products_images/${cartItem.image}`}
-                  alt={cartItem.itemname}
-                  loading="lazy"
-                />
-                <Stack ml={2} alignItems="flex-start">
-                  <Typography>{cartItem.itemname}</Typography>
-                  <Button
-                    variant="contained"
-                    color="error"
-                    onClick={() => handleRemoveFromCart(cartItem)}
-                  >
-                    Remove
-                  </Button>
-                </Stack>
-              </Grid>
-              <Grid item md={2} mt={2}>
-                <Typography>${cartItem.price}</Typography>
-              </Grid>
-              <Grid item md={4} mt={2}>
-                <Button
-                  sx={{
-                    bgcolor: "transparent",
-                    color: "black",
-                    border: "1px solid black",
-                    "&:hover": {
-                      backgroundColor: "transparent",
-                      border: "1px solid black",
-                    },
-                  }}
-                  disableRipple
-                  disableElevation
-                  disableFocusRipple
-                  disableTouchRipple
-                  startIcon={
-                    <IconButton onClick={() => handleDecreaseCart(cartItem)}>
-                      <RemoveIcon />
-                    </IconButton>
-                  }
-                  endIcon={
-                    <IconButton
-                      onClick={() => handleIncreaseQuantity(cartItem)}
-                    >
-                      <AddIcon />
-                    </IconButton>
-                  }
-                  variant="outlined"
-                >
-                  {cartItem.cartQuantity}
-                </Button>
-              </Grid>
-              <Grid item md={2} mt={2}>
-                <Typography>
-                  ${cartItem.price * cartItem.cartQuantity}
-                </Typography>
-              </Grid>
-            </>
-          ))}
-        </Grid>
-        <Divider />
-        <Grid container mt={3} mb={3}>
-          <Grid item md={4}>
-            <Button
-              variant="outlined"
-              onClick={() => handleEmptyCart()}
-              sx={{
-                bgcolor: "transparent",
-                color: "black",
-                border: "1px solid black",
-                "&:hover": {
-                  backgroundColor: "transparent",
-                  border: "1px solid black",
-                },
-              }}
-            >
-              Clear Cart
-            </Button>
-          </Grid>
-          <Grid item md={4}></Grid>
-          <Grid item md={4} flexWrap>
-            <Grid container>
-              <Grid item md={6}>
-                <Typography>Subtotal</Typography>
-              </Grid>
-              <Grid item md={6}>
-                <Typography>${cart.cartTotalAmount}</Typography>
-              </Grid>
-            </Grid>
-            <Button
-              variant="contained"
-              sx={{ width: "60%" }}
-              component={Link}
-              to="/checkout"
-            >
-              Checkout
-            </Button>
-          </Grid>
-        </Grid>
+        <Stack
+          my={3}
+          direction="row"
+          justifyContent={"space-between"}
+          alignItems="start"
+        >
+          <Stack gap={2} flexGrow={1}>
+            {cart.cartItems?.map((cartItem) => (
+              <CartItem cartItem={cartItem}></CartItem>
+            ))}
+          </Stack>
+          <Box
+            flexGrow={1}
+            sx={{
+              bgcolor: "rgba(255,255,255,150)",
+              position: "sticky",
+              top: "85px",
+            }}
+            p={2}
+          >
+            <Typography variant="h6" sx={line}>
+              Order Summary
+            </Typography>
+            <Typography variant="body1" mt={2}>
+              Total Items: {cart.cartTotalQuantity}
+            </Typography>
+            <Typography variant="body1">
+              SubTotal: ${cart.cartTotalAmount}
+            </Typography>
+            <Box width="100%" sx={line} mt={2} />
+            <Stack direction="row" mt={5} gap={2}>
+              <Button
+                variant="contained"
+                color="error"
+                onClick={() => handleEmptyCart()}
+                sx={{
+                  flexGrow: "1",
+                  
+                }}
+              >
+                Clear Cart
+              </Button>
+              <Button
+                variant="contained"
+                component={Link}
+                to="/checkout"
+                sx={{
+                  flexGrow: "1",
+                }}
+              >
+                Checkout
+              </Button>
+            </Stack>
+          </Box>
+        </Stack>
       </>
     );
   };
---
+++ src/pages/Cart/components/CartItem.jsx
@@ -0,0 +1,134 @@
+import React from "react";
+import {
+  Box,
+  Stack,
+  Typography,
+  Button,
+  IconButton,
+  Grid,
+} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import { useDispatch } from "react-redux";
+import {
+  addToCart,
+  decreaseCart,
+  removeFromCart,
+} from "../../../redux/cartSlice";
+import CloseIcon from "@mui/icons-material/Close";
+
+const CartItem = ({ cartItem }) => {
+  const dispatch = useDispatch();
+
+  const handleRemoveFromCart = (cartItem) => {
+    dispatch(removeFromCart(cartItem));
+  };
+
+  const handleDecreaseCart = (cartItem) => {
+    dispatch(decreaseCart(cartItem));
+  };
+
+  const handleIncreaseQuantity = (cartItem) => {
+    dispatch(addToCart(cartItem));
+  };
+
+  return (
+    <Box width="90%">
+      <Stack
+        direction="row"
+        sx={{
+          backgroundColor: "white",
+          boxShadow: "5px 5px 8px grey",
+          width: "100%",
+        }}
+        p={4}
+      >
+        <Box
+          sx={{
+            paddingTop: "15vw",
+            position: "relative",
+            overflow: "hidden",
+            width: "15vw",
+          }}
+          bgcolor="#f2f2f2!important"
+        >
+          <img
+            src={`http://localhost:8000/storage/products_images/${cartItem.image}`}
+            alt={cartItem.itemname}
+            style={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              maxWidth: "80%",
+              maxHeight: "80%",
+              transform: "translate(-50%, -50%)",
+            }}
+            loading="lazy"
+          />
+        </Box>
+
+        <Stack
+          ml={2}
+          alignItems="flex-start"
+          justifyContent="space-between"
+          width="100%"
+        >
+          <Stack justifyContent="space-between" direction="row" width="100%">
+            <Typography variant="h5">{cartItem.itemname}</Typography>
+            <IconButton onClick={() => handleRemoveFromCart(cartItem)}>
+              <CloseIcon></CloseIcon>
+            </IconButton>
+          </Stack>
+          <Stack>
+            <Stack direction="row" gap={2}>
+              <Typography variant="body1">
+                Quantity: {cartItem.cartQuantity}
+              </Typography>
+              <Typography variant="body1">Price: ${cartItem.price}</Typography>
+            </Stack>
+            <Typography variant="h6">
+              Subtotal: ${cartItem.price * cartItem.cartQuantity}
+            </Typography>
+            <Button
+              // sx={{
+              //   height: "3rem",
+              //   bgcolor: "#3396f5",
+              //   color: "white",
+              //   "&:hover": {
+              //     backgroundColor: "#3396f5",
+              //   },
+              // }}
+              disableRipple
+              disableElevation
+              disableFocusRipple
+              disableTouchRipple
+              startIcon={
+                <IconButton
+                  onClick={() => handleDecreaseCart(cartItem)}
+                  sx={{
+                    color: "white",
+                  }}
+                >
+                  <RemoveIcon />
+                </IconButton>
+              }
+              endIcon={
+                <IconButton
+                  sx={{ color: "white" }}
+                  onClick={() => handleIncreaseQuantity(cartItem)}
+                >
+                  <AddIcon />
+                </IconButton>
+              }
+              variant="contained"
+            >
+              {cartItem.cartQuantity}
+            </Button>
+          </Stack>
+        </Stack>
+      </Stack>
+    </Box>
+  );
+};
+
+export default CartItem;
